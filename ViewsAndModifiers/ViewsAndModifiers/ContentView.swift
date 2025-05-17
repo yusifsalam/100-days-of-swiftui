c1b9ade01916e8e18b68223a2ddba81b415bbaa1
Yusif Salam-zade <42574232+yusifsalam@users.noreply.github.com>
2025-05-17 13:59:30 +0300

    Day 23: ViewsAndModifiers init

---
+++ ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
@@ -0,0 +1,55 @@
+import SwiftUI
+
+struct Title: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.largeTitle)
+            .foregroundStyle(.white)
+            .padding()
+            .background(.blue)
+            .clipShape(.rect(cornerRadius: 10))
+    }
+}
+
+extension View {
+    func titleStyle() -> some View {
+        modifier(Title())
+    }
+}
+
+struct Watermark: ViewModifier {
+    var text: String
+    
+    func body(content: Content) -> some View {
+        ZStack(alignment: .bottomTrailing) {
+            content
+            Text(text)
+                .font(.caption)
+                .foregroundStyle(.white)
+                .padding(5)
+                .background(.black)
+        }
+    }
+}
+
+extension View {
+    func watermarked(with text: String) -> some View {
+        modifier(Watermark(text: text))
+    }
+}
+
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+            Color.blue
+            Text("Hello world")
+                .titleStyle()
+        }
+        .frame(width: 300, height: 200)
+        .watermarked(with: "Hacking with Swift")
+    }
+}
+
+#Preview {
+    ContentView()
+}
