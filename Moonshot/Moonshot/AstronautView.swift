603f7b7aaeaa0bbfa65955ebd8e9e6b27927ab6f
Yusif Salam-zade <42574232+yusifsalam@users.noreply.github.com>
2025-06-08 12:06:13 +0300

    Day 41: Moonshot impl

---
+++ Moonshot/Moonshot/AstronautView.swift
@@ -0,0 +1,27 @@
+import SwiftUI
+
+struct AstronautView: View {
+    let astronaut: Astronaut
+    
+    var body: some View {
+        ScrollView {
+            VStack {
+                Image(astronaut.id)
+                    .resizable()
+                    .scaledToFit()
+                
+                Text(astronaut.description)
+                    .padding()
+            }
+        }
+        .background(.darkBackground)
+        .navigationTitle(astronaut.name)
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+#Preview {
+    let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
+    AstronautView(astronaut: astronauts["aldrin"]!)
+        .preferredColorScheme(.dark)
+}
