118b743a3a437566ba928e0e21ddad5263bf32c8
Yusif Salam-zade <42574232+yusifsalam@users.noreply.github.com>
2025-06-20 18:49:41 +0300

    Day 53: Bookworm init

---
+++ Bookworm/Bookworm/Student.swift
@@ -0,0 +1,13 @@
+import Foundation
+import SwiftData
+
+@Model
+class Student {
+    var id: UUID
+    var name: String
+
+    init(id: UUID, name: String) {
+        self.id = id
+        self.name = name
+    }
+}
