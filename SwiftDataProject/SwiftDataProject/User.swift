106fc40052b3ca6a4be1bf3316996f234116eff7
Yusif Salam-zade <42574232+yusifsalam@users.noreply.github.com>
2025-06-21 08:39:23 +0300

    Day 57: SwiftDataProject init

---
+++ SwiftDataProject/SwiftDataProject/User.swift
@@ -0,0 +1,15 @@
+import Foundation
+import SwiftData
+
+@Model
+class User {
+    var name: String
+    var city: String
+    var joinDate: Date
+    
+    init(name: String, city: String, joinDate: Date) {
+        self.name = name
+        self.city = city
+        self.joinDate = joinDate
+    }
+}
