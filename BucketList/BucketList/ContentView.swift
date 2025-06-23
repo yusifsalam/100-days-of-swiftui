43d86a8747dd9e342a4b1933c76753894106ddf5
Yusif Salam-zade <42574232+yusifsalam@users.noreply.github.com>
2025-06-23 19:03:30 +0300

    Day 70: BucketList impl

--- BucketList/BucketList/ContentView.swift
+++ BucketList/BucketList/ContentView.swift
@@ -2,97 +2,51 @@ import LocalAuthentication
 import MapKit
 import SwiftUI
 
-struct Location: Identifiable {
-    let id = UUID()
-    var name: String
-    var coordinate: CLLocationCoordinate2D
-}
 
 struct ContentView: View {
-    @State private var isUnlocked = false
-    @State private var position = MapCameraPosition.region(
+    @State private var locations = [Location]()
+    @State private var selectedPlace: Location?
+    
+    let startPosition = MapCameraPosition.region(
         MKCoordinateRegion(
-            center: CLLocationCoordinate2D(latitude: 51.507222, longitude: -0.1275),
-            span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1)
+            center: CLLocationCoordinate2D(latitude: 56, longitude: -3),
+            span: MKCoordinateSpan(latitudeDelta: 10, longitudeDelta: 10)
         )
     )
-    let locations = [
-        Location(name: "Buckingham Palace", coordinate: CLLocationCoordinate2D(latitude: 51.501, longitude: -0.141)),
-        Location(name: "Tower of London", coordinate: CLLocationCoordinate2D(latitude: 51.508, longitude: -0.076))
-    ]
     var body: some View {
-        VStack {
-            if isUnlocked {
-                
-                MapReader { proxy in
-                    Map {
-                        ForEach(locations) { location in
-                            Annotation(location.name, coordinate: location.coordinate) {
-                                Text(location.name)
-                                    .font(.headline)
-                                    .padding()
-                                    .background(.blue)
-                                    .foregroundStyle(.white)
-                                    .clipShape(.capsule)
+        MapReader { proxy in
+            Map(initialPosition: startPosition) {
+                ForEach(locations) { location in
+                    Annotation(location.name, coordinate: location.coordinate) {
+                        Image(systemName: "star.circle")
+                            .resizable()
+                            .foregroundStyle(.red)
+                            .frame(width: 44, height: 44)
+                            .background(.white)
+                            .clipShape(.circle)
+                            .onTapGesture {
+                                selectedPlace = location
                             }
-                            .annotationTitles(.hidden)
-                        }
-                    }
-                    .onTapGesture { position in
-                        if let coordinate = proxy.convert(position, from: .local) {
-                            print(coordinate)
-                        }
                     }
                 }
-                
-                
-                HStack(spacing: 50) {
-                    Button("Paris") {
-                        position = MapCameraPosition.region(
-                            MKCoordinateRegion(
-                                center: CLLocationCoordinate2D(latitude: 48.8566, longitude: 2.3522),
-                                span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1)
-                            )
-                        )
-                    }
-                    
-                    Button("Tokyo") {
-                        position = MapCameraPosition.region(
-                            MKCoordinateRegion(
-                                center: CLLocationCoordinate2D(latitude: 35.6897, longitude: 139.6922),
-                                span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1)
-                            )
-                        )
+            }
+            .onTapGesture { position in
+                if let coordinate = proxy.convert(position, from: .local) {
+                    print("Tapped at \(coordinate)")
+                    let newLocation = Location(id: UUID(), name: "New location", description: "", latitude: coordinate.latitude, longitude: coordinate.longitude)
+                    locations.append(newLocation)
+                }
+            }
+            .sheet(item: $selectedPlace) { place in
+                EditView(location: place) { newLocation in
+                    if let index = locations.firstIndex(of: place) {
+                        locations[index] = newLocation
                     }
                 }
-            } else {
-                Text("Locked")
             }
         }
-        .onAppear(perform: authenticate)
     }
     
-    func authenticate() {
-        let context = LAContext()
-        var error: NSError?
-        
-        // check whether biometric authentication is possible
-        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
-            // it's possible, so go ahead and use it
-            let reason = "We need to unlock your data."
-            
-            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { success, authenticationError in
-                // authentication has now completed
-                if success {
-                    isUnlocked = true
-                } else {
-                    // there was a problem
-                }
-            }
-        } else {
-            // no biometrics
-        }
-    }
 }
 
 #Preview {
