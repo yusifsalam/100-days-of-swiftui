7334a2675cb1beb0cd4d0d4cd6f003a0d335ccc5
Yusif Salam-zade <42574232+yusifsalam@users.noreply.github.com>
2025-06-24 14:56:26 +0300

    Day 75: GuessTheFlag a11y

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -12,6 +12,20 @@ struct FlagImage: View {
 
 struct ContentView: View {
     @State var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Spain", "UK", "Ukraine", "US"].shuffled()
+    let labels = [
+        "Estonia": "Flag with three horizontal stripes. Top stripe blue, middle stripe black, bottom stripe white.",
+        "France": "Flag with three vertical stripes. Left stripe blue, middle stripe white, right stripe red.",
+        "Germany": "Flag with three horizontal stripes. Top stripe black, middle stripe red, bottom stripe gold.",
+        "Ireland": "Flag with three vertical stripes. Left stripe green, middle stripe white, right stripe orange.",
+        "Italy": "Flag with three vertical stripes. Left stripe green, middle stripe white, right stripe red.",
+        "Nigeria": "Flag with three vertical stripes. Left stripe green, middle stripe white, right stripe green.",
+        "Poland": "Flag with two horizontal stripes. Top stripe white, bottom stripe red.",
+        "Spain": "Flag with three horizontal stripes. Top thin stripe red, middle thick stripe gold with a crest on the left, bottom thin stripe red.",
+        "UK": "Flag with overlapping red and white crosses, both straight and diagonally, on a blue background.",
+        "Ukraine": "Flag with two horizontal stripes. Top stripe blue, bottom stripe yellow.",
+        "US": "Flag with many red and white stripes, with white stars on a blue background in the top-left corner."
+    ]
+    
     @State var correctAnswer = Int.random(in: 0...2)
     @State private var showingScore = false
     @State private var scoreTitle = ""
@@ -54,6 +68,7 @@ struct ContentView: View {
                                 .opacity(tappedFlagIndex == number ? 1: 2 - opacityAnimationAmount)
                                 .animation(.easeInOut(duration: 0.2), value: opacityAnimationAmount)
                                 .scaleEffect(tappedFlagIndex == number ? 1 : scaleAnimationAmount)
+                                .accessibilityLabel(labels[countries[number], default: "Unknown flag"])
                                 
                         }
                     }
