745706a7106b7450cba612c34929ab750d19f230
Yusif Salam-zade <42574232+yusifsalam@users.noreply.github.com>
2025-07-19 20:11:20 +0300

    Day 94: Layout challenges

--- LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
+++ LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
@@ -2,28 +2,31 @@ import SwiftUI
 
 
 struct ContentView: View {
+    
     var body: some View {
-        ScrollView(.horizontal, showsIndicators: false) {
-            HStack(spacing: 0) {
-                ForEach(1..<20) { num in
-                    Text("Number \(num)")
-                        .font(.largeTitle)
-                        .padding()
-                        .background(.red)
-                        .frame(width: 200, height: 200)
-                        .visualEffect { content, proxy in
-                            content
-                                .rotation3DEffect(.degrees(-proxy.frame(in: .global).minX) / 8, axis: (x: 0, y: 1, z: 0))
-                        }
-
+        GeometryReader { fullView in
+            ScrollView(.vertical) {
+                ForEach(0..<50) { index in
+                    GeometryReader { proxy in
+                        let hueShift = (proxy.frame(in: .global).minY / 800.0 + Double(index) * 0.05)
+                            .truncatingRemainder(dividingBy: 1.0)
+                        
+                        Text("Row #\(index)")
+                            .font(.title)
+                            .frame(maxWidth: .infinity)
+                            .background(
+                                Color(hue: hueShift, saturation: 0.7, brightness: 0.9)
+                            )
+                            .opacity(min(1, (proxy.frame(in: .global).minY - 80 ) / 200.0))
+                            .rotation3DEffect(.degrees(proxy.frame(in: .global).minY - fullView.size.height / 2) / 5, axis: (x: 0, y: 1, z: 0))
+                            .scaleEffect(max(0.5, min(1.5, proxy.frame(in: .global).minY / 600.0 )))
+                    }
+                    .frame(height: 40)
                 }
             }
-            .scrollTargetLayout()
         }
-        .scrollTargetBehavior(.viewAligned)
     }
 }
-
 
 #Preview {
     ContentView()
