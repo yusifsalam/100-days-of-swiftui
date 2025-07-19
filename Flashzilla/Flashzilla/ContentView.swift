c1ac55b9c376d10130b7b302a49d35d90508404a
Yusif Salam-zade <42574232+yusifsalam@users.noreply.github.com>
2025-07-19 10:41:05 +0300

    Day 87: Flashzilla tech

--- Flashzilla/Flashzilla/ContentView.swift
+++ Flashzilla/Flashzilla/ContentView.swift
@@ -1,25 +1,59 @@
 import SwiftUI
 
 struct ContentView: View {
+    @Environment(\.scenePhase) var scenePhase
+    @Environment(\.accessibilityDifferentiateWithoutColor) var differentiateWithoutColor
+    @Environment(\.accessibilityReduceTransparency) var reduceTransparency
+    @State private var scale = 1.0
+    
+    
     var body: some View {
-        ZStack {
-            Rectangle()
-                .fill(.blue)
-                .frame(width: 300, height: 300)
-                .onTapGesture {
-                    print("Rectangle tapped!")
+        HStack {
+            if differentiateWithoutColor {
+                Image(systemName: "checkmark.circle")
+            }
+            
+            Text("Success")
+        }
+        .padding()
+        .background(differentiateWithoutColor ? .black : .green)
+        .foregroundStyle(.white)
+        .clipShape(.capsule)
+        
+        Text("Hello, world!")
+            .onChange(of: scenePhase) { oldPhase, newPhase in
+                if newPhase == .active {
+                    print("Active")
+                } else if newPhase == .inactive {
+                    print("Inactive")
+                } else if newPhase == .background {
+                    print("Background")
                 }
-
-            Circle()
-                .fill(.red)
-                .frame(width: 300, height: 300)
-                .contentShape(.rect)
-                .onTapGesture {
-                    print("Circle tapped!")
-                }
+            }
+        Button("Hello, World!") {
+            withOptionalAnimation {
+                scale *= 1.5
+            }
+            
+        }
+        .scaleEffect(scale)
+        
+        Text("Hello, World!")
+            .padding()
+            .background(reduceTransparency ? .black : .black.opacity(0.5))
+            .foregroundStyle(.white)
+            .clipShape(.capsule)
+    }
+    
+    func withOptionalAnimation<Result>(_ animation: Animation? = .default, _ body: () throws -> Result) rethrows -> Result {
+        if UIAccessibility.isReduceMotionEnabled {
+            return try body()
+        } else {
+            return try withAnimation(animation, body)
         }
     }
 }
+
 #Preview {
     ContentView()
 }
